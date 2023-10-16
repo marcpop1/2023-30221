396cce19442516bd644aa704e84b841376a188a9
MagSebi <sebastian18mag@gmail.com>
2023-10-16 21:57:19 +0300

    My commit message

---
+++ Students/Mag Sebastian - Mihai/src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+//import
+
+public class Main {
+	int a=0;
+	int b=0;
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Word");
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+		//for(int i=0;i<args.length;i++)
+			//System.out.println(args[i]);
+	}
+
+}
