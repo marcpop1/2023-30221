76d40f9971fc4b23ddfb4e548fd1d6962e045da7
Muntean Bogdan <bogdan.utu@yahoo.com>
2023-10-16 22:03:06 +0300

    My commit message

---
+++ Students/Muntean Utu - Bogdan/MainClass.java
@@ -0,0 +1,11 @@
+package main;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		for(int i=0;i<args.length;i++)
+			System.out.println(args[i]);
+	}
+
+}
