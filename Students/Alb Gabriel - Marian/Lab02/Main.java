238182a1fdfe47a665528b5698e1b47a8b0fe2de
GabrielAlb <alb.gabriel567@gmail.com>
2023-10-18 14:43:10 +0300

    commit

--- Students/Alb Gabriel - Marian/Lab02/Main.java
+++ Students/Alb Gabriel - Marian/Lab02/Main.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
+    int a=0;
+    int b=0;
+    void doSomething(){
+        a=1;
+        b=2;
+    }
+
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         System.out.println("Enter the name, age and salary:");
@@ -12,5 +20,9 @@ public class Main {
         System.out.println("Age: " + age);
         System.out.println("Salary: " + salary);
 
+        for(int i=0; i<args.length; i++){
+            System.out.println(args[i]);
+        }
+        System.out.println("Hello World!");
     }
 }
