1bd305dd11f1320de16a89f9a27d3bc43b2eacf4
Marc <marcpop2003@yahoo.com>
2023-10-28 21:45:21 +0300

    Added lab4 work

---
+++ Students/Pop Marc - Alexandru/Lab4/src/OperationProduct.java
@@ -0,0 +1,22 @@
+public class OperationProduct {
+    private Product product;
+
+    private int quantity;
+
+    public OperationProduct(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
