8e94bd781bdd0a7c9e1bb6f9dcf4b6a924b76298
truta193 <trutaandrei7033@gmail.com>
2023-11-09 20:09:37 +0200

    Modified lab05

--- Students/Truta Andrei/Lab05/src/ro/utcn/BankAccount.java
+++ Students/Truta Andrei/Lab05/src/ro/utcn/BankAccount.java
@@ -4,34 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BankAccount {
-
-    //TODO: Do you think it's a good idea to have this enum here?
-    //TODO: Would a separate class be better?
-    public static enum AccountType {
-        CURRENT, SAVINGS
-    }
-
-    //TODO: Is this a good way to handle the id?
-    //TODO: As in, is it a good idea to have it as a static field?
-    //TODO: The idea is to have a way to keep track of the used ids across all instances of BankAccount
-    public static Integer nextId = -1;
+    private static Integer nextId = -1;
     private Integer accountNumber;
     private AccountType accountType;
     private Double balance;
     private List<Transaction> transactions = new ArrayList<Transaction>();
 
+    private Integer getNextId() {
+        return ++nextId;
+    }
+
     public BankAccount(AccountType accountType) {
         this.accountType = accountType;
         this.balance = 0.0;
-        this.accountNumber = ++nextId;
+        this.accountNumber = getNextId();
     }
 
     public Integer getAccountNumber() {
         return this.accountNumber;
     }
 
-    //TODO: How do you handle cases where the user input is invalid in an actual app?
-    //TODO: Is throwing an exception a good idea or is there a better way? Should I just ignore the input or print an error message?
     public void depositCurrency(Double amount) {
         if (amount < 0) {
             throw new IllegalArgumentException("Amount cannot be negative");
@@ -50,18 +42,14 @@ public class BankAccount {
     }
 
     public void addTransaction(Transaction transaction) {
-        //TODO: Duplicate branches but it's easier to read, is it worth it?
+
         switch (transaction.getTransactionType()) {
+            case TRANSFER_IN:
             case DEPOSIT:
                 this.balance += transaction.getAmount();
                 break;
-            case WITHDRAWAL:
-                this.balance -= transaction.getAmount();
-                break;
-            case TRANSFER_IN:
-                this.balance += transaction.getAmount();
-                break;
             case TRANSFER_OUT:
+            case WITHDRAWAL:
                 this.balance -= transaction.getAmount();
                 break;
         }
