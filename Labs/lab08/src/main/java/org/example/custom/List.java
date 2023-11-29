ce4464841da2b067289f14a1c5023a3d9ff09905
AlexBondor <alexandru.viorel.bondor@gmail.com>
2023-11-29 08:40:01 +0200

    added lab08

---
+++ Labs/lab08/src/main/java/org/example/custom/List.java
@@ -0,0 +1,28 @@
+package org.example.custom;
+
+public class List<T extends ListableObject> {
+
+  Object[] list = new Object[10]; // wrapper
+
+  public void add(T item) {
+    for (int i = 0; i < list.length; i++) {
+      if (list[i] == null) {
+        list[i] = item;
+        break;
+      }
+    }
+  }
+
+  public void remove(T item) {
+    for (int i = 0; i < list.length; i++) {
+      if (list[i] != null && list[i].equals(item)) {
+        list[i] = null;
+        break;
+      }
+    }
+  }
+
+  public T get(int index) {
+    return (T) list[index];
+  }
+}
---
+++ Labs/lab08/src/main/java/org/example/custom/ListableObject.java
@@ -0,0 +1,6 @@
+package org.example.custom;
+
+public interface ListableObject {
+
+  void doSomething();
+}
